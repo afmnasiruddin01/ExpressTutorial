35784ee1f7948ba600ee17ac9ea94d6eabd1c82c
afmnasiruddin01 <afmnasiruddin01@gmail.com>
2026-04-05 11:31:12 +0600

    createPost function has been added for browser

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const output = document.querySelector("#output");
 const button = document.querySelector("#get-btn-posts");
+const btnPost = document.querySelector("#submit");
+const form = document.querySelector("#add-post-form");
 
 async function getPostsHtml() {
     try {
@@ -21,4 +23,35 @@ async function getPostsHtml() {
 
     
 }
+
+// POST data from form
+async function createPost(event){
+    event.preventDefault();
+    const name = form.elements["name"].value;
+    try {
+        const response = await fetch("http://localhost:3000/api/posts",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name })
+
+        });
+        
+        if(response.ok){
+            // Append the new post to the output
+            const pl = document.createElement("div");
+            pl.textContent = name;
+            output.appendChild(pl);
+            
+            // Clear the form input
+            form.elements["name"].value = "";
+        }
+
+} catch (e){
+    throw new Error(e)
+}
+}
+
+form.addEventListener("submit", createPost);
 button.addEventListener("click",getPostsHtml);
