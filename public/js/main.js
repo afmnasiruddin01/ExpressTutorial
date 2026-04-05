0f4f73c03f9beaa8060f360814b0612dc5544dd1
afmnasiruddin01 <afmnasiruddin01@gmail.com>
2026-04-05 15:54:09 +0600

    revision in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
-const output = document.querySelector("#output");
-const button = document.querySelector("#get-btn-posts");
-const btnPost = document.querySelector("#submit");
 const form = document.querySelector("#add-post-form");
+const btnPost = document.querySelector("#submit");
+const button = document.querySelector("#get-btn-posts");
+const output = document.querySelector("#output");
 
 async function getPostsHtml() {
     try {
@@ -27,7 +27,10 @@ async function getPostsHtml() {
 // POST data from form
 async function createPost(event){
     event.preventDefault();
-    const name = form.elements["name"].value;
+   
+    const formData = new FormData(this);
+    const name= formData.get("name");    
+
     try {
         const response = await fetch("http://localhost:3000/api/posts",{
             method: "POST",
@@ -41,12 +44,13 @@ async function createPost(event){
         if(response.ok){
             // Append the new post to the output
             const pl = document.createElement("div");
-            pl.textContent = name;
+            pl.textContent = response.json().name; // Assuming the response contains the created post with a 'name' property
             output.appendChild(pl);
             
             // Clear the form input
-            form.elements["name"].value = "";
+            //form.elements["name"].value = "";
         }
+        getPostsHtml(); // Refresh the posts list after adding a new post
 
 } catch (e){
     throw new Error(e)
